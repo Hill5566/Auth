1ba16328d9f229dd1467ae7e60336c0d8ecba3e4
Hill <angelblue065@gmail.com>
2022-12-07 20:53:03 +0800

    feat : new project

---
+++ Auth/Auth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auth
+//
+//  Created by Lin Hill on 2022/12/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
