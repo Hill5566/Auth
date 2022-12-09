22b0699d3a2699e1ccda0608f5fcd6a9c8544d83
Hill <angelblue065@gmail.com>
2022-12-09 13:35:11 +0800

    feat : 成功取得臉書資料

--- Auth/Auth/AppDelegate.swift
+++ Auth/Auth/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FacebookCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+
+        ApplicationDelegate.shared.application(
+                    application,
+                    didFinishLaunchingWithOptions: launchOptions
+                )
+
         return true
     }
 
--- Auth/Auth/ViewController.swift
+++ Auth/Auth/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import FacebookLogin
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let loginButton = FBLoginButton()
+        loginButton.center = view.center
+        view.addSubview(loginButton)
+        
+        if let token = AccessToken.current,
+                !token.isExpired {
+                // User is logged in, do work such as go to next view controller.
+            }
+        
+        
+        // Extend the code sample from 6a. Add Facebook Login to Your Code
+        // Add to your viewDidLoad method:
+        loginButton.permissions = ["public_profile", "email"]
+          
     }
-
-
+    @IBAction func request(_ sender: UIButton) {
+        
+        if AccessToken.current != nil {
+            GraphRequest(graphPath: "me").start { connection, result, error in
+                if let result = result {
+                    print("Fetched Result: \(result)")
+                }
+            }
+        }
+        
+    }
+    
 }
 
